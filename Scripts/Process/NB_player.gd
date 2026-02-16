d3b251b4f30ed8d5775841cc26ea4cca4ecd51b5
officialErnests <n0o0b090lv@gmail.com>
2026-02-16 23:21:12 +0200

    what have you done..

--- Scripts/Process/NB_player.gd
+++ Scripts/Process/NB_player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-@export var speed: float = 10
+@export var speed: float = 100
+@export var label: Label
 @export_category("swings")
 @export var swing_cooldown: float = 0.5
 @export var swing_dmg: float = 1
@@ -10,6 +11,7 @@ extends CharacterBody2D
 @export var swing_area: Area2D
 var swing_timer = 0
 var timer: float = 0
+var slimes_murdered = 0
 
 func _process(delta: float) -> void:
 	timer += delta
@@ -22,11 +24,18 @@ func _process(delta: float) -> void:
 		swing_node.look_at(cursor_global)
 		swing_animation_player.play("swing")
 		velocity += cursor_global - global_position
-		for enemie: NB_enemie in swing_area.get_overlapping_bodies():
-			enemie.dmg(swing_dmg)
+		dmg_twice()
+
+func dmg_twice():
+	for enemie: NB_enemie in swing_area.get_overlapping_bodies():
+			if enemie.dmg(swing_dmg):
+				slimes_murdered += 1
+				label.text = "INOCENT slimes murdered: " + str(slimes_murdered) + "/100"
+				if slimes_murdered > 99:
+					get_tree().change_scene_to_file("res://scenes/win.tscn")
 
 func _physics_process(delta: float) -> void:
 	var movement: Vector2 = Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down"))
 	velocity += movement.normalized() * delta * speed
-	velocity *= 0.9
+	velocity *= 0.95
 	move_and_slide()
